859b07c77808433ad37d67a76e5c16aa94ecad54
xioperez01 <angie.xioperez@gmail.com>
2021-06-22 11:45:13 -0500

    Closure and loops

---
+++ 0x0C-Javascript_advanced/3-classrooms.js
@@ -0,0 +1,17 @@
+function createClassRoom(numbersOfStudents) {
+  function studentSeat(seat) {
+    return (func = () => {
+      return seat;
+    });
+  }
+
+  let students = new Array();
+
+  for (let i = 0; i < numbersOfStudents; i++) {
+    students.push(studentSeat(i + 1));
+  }
+
+  return students;
+}
+
+const classRoom = createClassRoom(10);
