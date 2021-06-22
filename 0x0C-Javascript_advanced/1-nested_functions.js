bc243ca6cef6d9952e1705c7dba90d96a3429fbd
xioperez01 <angie.xioperez@gmail.com>
2021-06-22 11:20:23 -0500

    Closure Scope Chain

---
+++ 0x0C-Javascript_advanced/1-nested_functions.js
@@ -0,0 +1,22 @@
+const welcome = "Welcome";
+
+function outer() {
+	alert(welcome);
+
+	const course = "Holberton";
+
+	function inner() {
+		alert(welcome + " " + course);
+
+		const exclamation = "!";
+
+		function inception() {
+			alert(welcome + " " + course + " " + exclamation);
+		}
+
+		inception();
+	}
+	inner();
+}
+
+outer();
