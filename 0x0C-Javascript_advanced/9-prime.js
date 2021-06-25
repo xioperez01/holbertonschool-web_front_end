4b8601c171cbc747f57427977be1db1dd1d95379
xioperez01 <angie.xioperez@gmail.com>
2021-06-25 16:08:38 -0500

    Prime numbers & timing execution

---
+++ 0x0C-Javascript_advanced/9-prime.js
@@ -0,0 +1,26 @@
+function countPrimeNumbers() {
+  let counter = 0;
+  let isPrime;
+  for (let index = 2; index <= 100; index++) {
+    isPrime = true;
+    for (let j = 2; j <= index; j++) {
+      if (index % j === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+    if (isPrime) {
+      counter++;
+    }
+  }
+  return counter;
+}
+
+const initial = performance.now();
+countPrimeNumbers();
+const final = performance.now();
+console.log(
+  `Execution time of printing countPrimeNumbers was ${
+    final - initial
+  } milliseconds.`
+);
