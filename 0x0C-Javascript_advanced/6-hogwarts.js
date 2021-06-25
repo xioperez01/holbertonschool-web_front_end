82a838e9180c7faead332ce016ab5028588eb227
xioperez01 <angie.xioperez@gmail.com>
2021-06-25 14:14:25 -0500

    Private methods with closure

---
+++ 0x0C-Javascript_advanced/6-hogwarts.js
@@ -0,0 +1,36 @@
+function studentHogwarts() {
+  let privateScore = 0;
+  let name = null;
+
+  function changeScoreBy(points) {
+    privateScore += points;
+  }
+
+  return {
+    setName: (newName) => (name = newName),
+    rewardStudent: () => changeScoreBy(1),
+    penalizeStudent: () => changeScoreBy(-1),
+    getScore: () => {
+      return `${name}: ${privateScore}`;
+    },
+  };
+}
+
+// Harry
+const harry = new studentHogwarts();
+harry.setName('Harry');
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+console.log(harry.getScore());
+
+// Draco
+const draco = new studentHogwarts();
+draco.setName('Draco');
+draco.rewardStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+console.log(draco.getScore());
+
